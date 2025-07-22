a39bd9f0526df019aa3b0ca7f4ca62e109855920
Aso-Someya <147671523+Aso-Someya@users.noreply.github.com>
2025-07-22 17:49:22 +0900

    Update api.php

--- api.php
+++ api.php
@@ -3,8 +3,15 @@
 $msg_no = "学籍番号： ".$_POST['no'];
 $msg_score = "点数： ".$_POST['score'];
 
-echo json_encode(['messages' => [$msg_no, $msg_score]]);
+// 返却するデータを作成（PHPの連想配列）
+$data = [
+    'no' => $msg_no,
+    'score' => $msg_score
+];
+// 連想配列をJSON形式の文字列に変換して出力
+echo json_encode($data);
 exit;
 
 // 参考ページ
 // https://codelikes.com/jquery-php-ajax/
+?>
