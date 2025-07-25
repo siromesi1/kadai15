b0783df667944f0a87e4c8643af31ea9cd79be20
siromesi1 <2401202@s.asojuku.ac.jp>
2025-07-25 09:54:35 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,14 +4,15 @@ $(function() {
     $('#btn').on('click', function(){
 
         //入力データを取得
-
+        let input_no = $("[name='no']").val();
+        let input_score = $("[name='score']").val();
         $.ajax({
             //送信方法
-
+            type:'POST',
             //リクエスト先のURLを設定
-
+            url: 'api.php',
             //送信データの設定
-
+            data : {no:input_no, score:input_score},
         }).done(function(data){
 
             //JSONデータを解析
